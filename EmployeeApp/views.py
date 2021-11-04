05053aaa3860d3c951d4c4cb4a855424e04c8f96
Flavio Mendes <fhenrique609@gmail.com>
2021-11-04 15:35:29 -0300

    Upload Photo API

--- EmployeeApp/views.py
+++ EmployeeApp/views.py
@@ -6,6 +6,8 @@ from django.http.response import JsonResponse
 from EmployeeApp.models import Departments, Employees
 from EmployeeApp.serializers import DepartmentSerializer, EmployeeSerializer
 
+from django.core.files.storage import default_storage
+
 # Create your views here.
 
 @csrf_exempt
@@ -66,3 +68,11 @@ def employeeApi(request, id=0):
         employee = Employees.objects.get(EmployeeId=id)
         employee.delete()
         return JsonResponse("Delete Successfully!", safe=False)
+
+
+@csrf_exempt
+def SaveFile(request):
+    file = request.FILES['uploadedFile']
+    file_name = default_storage.save(file.name, file)
+
+    return JsonResponse(file_name, safe=False)
